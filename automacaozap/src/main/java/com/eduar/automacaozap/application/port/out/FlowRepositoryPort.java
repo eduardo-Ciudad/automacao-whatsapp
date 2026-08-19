72ba51ac94e6cc24f80cced68e392989ee90d441
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-08-19 18:03:50 -0300

    feat: add FlowRepositoryPort interface

---
+++ automacaozap/src/main/java/com/eduar/automacaozap/application/port/out/FlowRepositoryPort.java
@@ -0,0 +1,11 @@
+package com.eduar.automacaozap.application.port.out;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import com.eduar.automacaozap.domain.model.Flow;
+
+public interface FlowRepositoryPort {
+
+    Optional<Flow> findActiveById(UUID id);
+}
